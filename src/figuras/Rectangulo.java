bdd6765c291230e1d3cfe1009477eb7edb9361ca
Moreira Rubén Darío <moreirard@gmail.com>
2023-08-29 15:29:32 -0300

    Versión inicial de l proyecto Figura

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,11 @@
+package figuras;
+
+public class Rectangulo {
+
+	private Integer base, altura;
+
+	public Rectangulo(Integer base, Integer altura) {
+		this.base = base;
+		this.altura = altura;
+	}
+}
